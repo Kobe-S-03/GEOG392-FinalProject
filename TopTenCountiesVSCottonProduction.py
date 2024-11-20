7f4e25a09ab4c598ed1b809139c491607ca89d68
Kobe-S-03 <kschwausch26@gmail.com>
2024-11-20 13:33:16 -0600

    Create TopTenCountiesVSCottonProduction.py

---
+++ TopTenCountiesVSCottonProduction.py
@@ -0,0 +1,13 @@
+import arcpy
+aprx = arcpy.mp.ArcGISProject("current")
+map = aprx.listMaps()[0]
+newLayer = map.listLayers('Cotton_10')[0]
+chart = arcpy.Chart('MyChart')
+chart.type = 'bar'
+chart.title = 'Counties vs cotton production'
+chart.description = 'This chart displays the top 10 counties with most cotton production (2001-2021).'
+chart.xAxis.field = 'County'
+chart.yAxis.field = 'F__of_Bale'
+chart.xAxis.title = 'Counties'
+chart.yAxis.title = 'Cotton Production'
+chart.addToLayer(newLayer)
