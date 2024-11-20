1349f918e88ff8600dbb4b8822d6f7f012fd6b52
Kobe-S-03 <kschwausch26@gmail.com>
2024-11-20 13:38:55 -0600

    Update DroughtEventsBarChart.py

--- DroughtEventsBarChart.py
+++ DroughtEventsBarChart.py
@@ -1,10 +1,12 @@
 import arcpy
 
+#Select data
 aprx = arcpy.mp.ArcGISProject("current")
 map = aprx.listMaps()[0]
 newLayer = map.listLayers('Least_Drgt_events')[0]
 chart = arcpy.Chart('MyChart')
 
+#Design Bar Chart
 chart.type = 'bar'
 chart.title = 'Counties vs Drought Events'
 chart.description = 'This chart displays the top 10 counties with least drought events (2001-2021).'
